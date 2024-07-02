ddae57f9e0f55f8c27ecbc8c7e0ad54f8b649a92
Devin Martin <devin.martin.lpa@gmail.com>
2024-07-02 11:14:05 -0400

    DEBUG=True to debug Website. SQL scripts do nothing

--- DatabaseProject/settings.py
+++ DatabaseProject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if "RENDER" in os.environ else True
+DEBUG = True  #False if "RENDER" in os.environ else True
 
 ALLOWED_HOSTS = ['exercise-platform.onrender.com'] if "RENDER" in os.environ else ["*"]
 
